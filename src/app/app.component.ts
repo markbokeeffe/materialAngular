6c39d47863c922ae316b93252a52041a89b4f68f
markokeeffe <markokeeffe@vsware.ie>
2017-10-12 17:21:31 +0100

    Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  //title = 'app';
+  myData: {};
+
+  constructor(private http: HttpClient) {
+  /**this.http.get('https://jsonplaceholder.typicode.com/photos')
+    .then(function(response){
+      console.log('response')
+
+    });
+**/
+    //  .map(response => response.json())
+  //  .map((res: Response) => res.json())
+    //  .subscribe(res => this.myData = res);
+
+  }
+
+
+  ngOnInit(): void {
+    this.http.get('https://jsonplaceholder.typicode.com/photos').subscribe(data => {
+      console.log(data);
+      this.myData = data;
+      console.log(data);
+    });
+  }
+
+
 }
